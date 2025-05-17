a3836b67dfc5595f53a5775e2fd095e723e7fe34
Dave Oram <david.oram99@gmail.com>
2025-05-17 02:31:00 +0000

    Git pull on devcontainer post create

--- post_create_command.sh
+++ post_create_command.sh
@@ -5,3 +5,6 @@
 # - Runs inside the container once, after both onCreateCommand and updateContentCommand, and only when the container is first assigned to a user.
 # - It’s the last of the three creation hooks and can safely reference user‐specific secrets, credentials, or environment variables (e.g. installing packages from a private registry, initializing dotfiles). 
 #
+
+# Lets ensure that we have the latest code from the remote repository
+git pull origin

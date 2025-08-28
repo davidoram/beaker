8b976aa6da2643b2e700651a4a55dd20a97816ba
Dave Oram <david.oram99@gmail.com>
2025-08-28 09:40:04 +0000

    Tests

--- internal/utility/schema_loader_test.go
+++ internal/utility/schema_loader_test.go
@@ -1,11 +1,35 @@
 package utility
 
 import (
+	"bytes"
 	"path/filepath"
 	"testing"
 
+	"github.com/santhosh-tekuri/jsonschema/v6"
 	"github.com/stretchr/testify/require"
 )
+
+func Test_NewJSONSchemaCompiler(t *testing.T) {
+	compiler, err := NewJSONSchemaCompiler(t.Context(), filepath.Join(".", "..", "..", "schemas"))
+	require.NotNil(t, compiler)
+	require.NoError(t, err)
+
+	// Check it can compile a schema
+	schema, err := compiler.Compile("http://github.com/davidoram/beaker/schemas/stock-add.request.json")
+	require.NoError(t, err)
+
+	// Check the schema validates ok
+	validJSON := `{"product-sku": "abc-123", "quantity": 5}`
+	data, err := jsonschema.UnmarshalJSON(bytes.NewReader([]byte(validJSON)))
+	require.NoError(t, err)
+	require.NoError(t, schema.Validate(data))
+
+	// Check the schema detects invalid JSON
+	invalidJSON := `{"product-sku": "abc-123", "quantity": "five"}` // Note 'quantity' not a number
+	data, err = jsonschema.UnmarshalJSON(bytes.NewReader([]byte(invalidJSON)))
+	require.NoError(t, err)
+	require.Error(t, schema.Validate(data))
+}
 
 func TestNewLoader_HappyPath(t *testing.T) {
 	schemaDir := filepath.Join(".", "..", "..", "schemas")
